60f1d80d07662230c6089db6ba2465ef0b8a8972
Davor Babić <davor@davor.se>
2018-04-11 14:07:40 +0200

    refactor send_message

--- spec/fortytwoish/client_spec.rb
+++ spec/fortytwoish/client_spec.rb
@@ -5,11 +5,21 @@ RSpec.describe Fortytwoish::Client do
   let(:number) { '15415553010' }
   subject { Fortytwoish::Client.new(number, message).perform }
 
+  before do
+    Fortytwoish.configure do |config|
+      config.token = 'TESTTOKEN'
+    end
+  end
+
+  after do
+    Fortytwoish.reset_configuration
+  end
+
   context 'for successful sends' do
     before do
       stub_request(:post, 'https://rest.fortytwo.com/1/im').
         with(body: '{"destinations":[{"number":"15415553010"}],"sms_content":{"message":"hello, world"}}',
-             headers: { 'Authorization': 'Token ', 'Content-Type': 'application/json; charset=utf-8' }).
+             headers: { 'Authorization': 'Token TESTTOKEN', 'Content-Type': 'application/json; charset=utf-8' }).
         to_return(status: 200)
     end
 
@@ -20,12 +30,10 @@ RSpec.describe Fortytwoish::Client do
     before do
       stub_request(:post, 'https://rest.fortytwo.com/1/im').
         with(body: '{"destinations":[{"number":"15415553010"}],"sms_content":{"message":"hello, world"}}',
-             headers: { 'Authorization': 'Token ', 'Content-Type': 'application/json; charset=utf-8' }).
+             headers: { 'Authorization': 'Token TESTTOKEN', 'Content-Type': 'application/json; charset=utf-8' }).
         to_return(status: 400)
-
     end
 
     it { is_expected.to eq '400' }
   end
-
 end
